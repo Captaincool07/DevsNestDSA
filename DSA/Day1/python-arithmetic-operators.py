3c3403f25b73b2ba60f49834e47f33dfa712f88f
Captaincool07 <shindeamol181195@gmail.com>
2021-11-20 19:49:53 +0530

    first commit

---
+++ DSA/Day1/python-arithmetic-operators.py
@@ -0,0 +1,13 @@
+#https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    sum=a+b;
+    sub=a-b;
+    mul=a*b;
+    print(sum);
+    print(sub);
+    print(mul);
+    
